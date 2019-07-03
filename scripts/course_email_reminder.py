602abd8c51bf6ee6342fbe2a487049b69e632de9
Aaron Zhao <rudy710@qq.com>
2019-07-03 12:20:25 +0800

    Reminder: clear persistent grade, override when re-enroll (#92)

--- scripts/course_email_reminder.py
+++ scripts/course_email_reminder.py
@@ -13,6 +13,7 @@ again when the data matches
 from __future__ import division
 
 import logging
+import os
 import time
 from dateutil import relativedelta
 from logging.handlers import TimedRotatingFileHandler
@@ -21,8 +22,16 @@ from django.conf import settings
 from django.db.models import Q
 from django.utils import timezone
 
+from completion.models import BlockCompletion
 from courseware.models import StudentModule
 from instructor.enrollment import get_email_params, get_user_email_language, render_message_to_string
+from lms.djangoapps.grades.config.models import PersistentGradesEnabledFlag
+from lms.djangoapps.grades.models import (
+    PersistentCourseGrade,
+    PersistentCourseProgress,
+    PersistentSubsectionGrade,
+    PersistentSubsectionGradeOverride
+)
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 from student.models import CourseEnrollment
 from util.email_utils import send_mail_with_alias as send_mail
@@ -30,6 +39,11 @@ from xmodule.modulestore.django import modulestore
 
 
 logger = logging.getLogger("edx.scripts.course_email_reminder")
+
+d = os.path.dirname('/edx/var/log/cms/course_email_reminder.log')
+if not os.path.exists(d):
+    os.makedirs(d)
+
 log_handler = TimedRotatingFileHandler("/edx/var/log/cms/course_email_reminder.log",
                                        when="W0",
                                        backupCount=5,
@@ -53,7 +67,9 @@ def set_time(timez):
 
 def course_filter(course_id):
     descriptor = modulestore().get_course(course_id)
-    return len(descriptor.reminder_info) > 0
+    if descriptor:
+        return len(descriptor.reminder_info) > 0
+    return False
 
 
 def get_course_with_reminders():
@@ -63,7 +79,7 @@ def get_course_with_reminders():
 
     # filter the course that not end yet or doesn't have end date
     overviews = CourseOverview.objects.all().filter(Q(end__gte=TIME_NOW) | Q(end=None))
-    courses = [c.id for c in overviews if course_filter(c.id)]
+    courses = [c.id for c in overviews if course_filter(c.id) and c.has_started()]
     return courses
 
 
@@ -98,10 +114,81 @@ def course_re_enroll(course_enrollment):
     course_enrollment.save()
     student_modules = StudentModule.objects.filter(
         course_id=course_enrollment.course_id,
-        module_type='problem',
         student=course_enrollment.user
     )
     student_modules.delete()
+
+    if PersistentGradesEnabledFlag(course_enrollment.course_id):
+        try:
+            grade = PersistentCourseGrade.read(course_enrollment.user_id, course_enrollment.course_id)
+            grade.delete()
+            logger.info("reset course grade for user: {user_name}, course_id: {course_id}".format(
+                user_name=course_enrollment.user.username,
+                course_id=course_enrollment.course_id
+            ))
+        except Exception as e:
+            logger.error("failed to reset course grade for user: {user_name}, course_id: {course_id}, "
+                         "reason: {reason}".format(user_name=course_enrollment.user.username,
+                                                   course_id=course_enrollment.course_id,
+                                                   reason=e
+                                                   ))
+
+        try:
+            progress = PersistentCourseProgress.read(course_enrollment.user_id, course_enrollment.course_id)
+            progress.percent_progress = 0
+            progress.save()
+            logger.info("reset course progress for user: {user_name}, course_id: {course_id}".format(
+                user_name=course_enrollment.user.username,
+                course_id=course_enrollment.course_id
+            ))
+        except Exception as e:
+            logger.error("failed to reset course progress for user: {user_name}, course_id: {course_id}, "
+                         "reason: {reason}".format(user_name=course_enrollment.user.username,
+                                                   course_id=course_enrollment.course_id,
+                                                   reason=e
+                                                   ))
+
+        try:
+            completions = BlockCompletion.user_course_completion_queryset(
+                course_enrollment.user, course_enrollment.course_id
+            )
+            completions.update(completion=0)
+            logger.info("reset course page completions for user: {user_name}, course_id: {course_id}".format(
+                user_name=course_enrollment.user.username,
+                course_id=course_enrollment.course_id
+            ))
+        except Exception as e:
+            logger.error("failed to reset course page completions for user: {user_name}, course_id: {course_id}, "
+                         "reason: {reason}".format(user_name=course_enrollment.user.username,
+                                                   course_id=course_enrollment.course_id,
+                                                   reason=e
+                                                   ))
+
+        try:
+            subsection_grades = PersistentSubsectionGrade.bulk_read_grades(
+                course_enrollment.user_id, course_enrollment.course_id
+            )
+            for i in subsection_grades:
+                i.earned_all = 0
+                i.earned_graded = 0
+                i.first_attempted = None
+                i.save()
+                try:
+                    override = PersistentSubsectionGradeOverride.objects.get(grade=i)
+                    override.delete()
+                except PersistentSubsectionGradeOverride.DoesNotExist:
+                    pass
+
+            logger.info("reset course subsection grade for user: {user_name}, course_id: {course_id}".format(
+                user_name=course_enrollment.user.username,
+                course_id=course_enrollment.course_id
+            ))
+        except Exception as e:
+            logger.error("failed to reset course subsection grade for user: {user_name}, course_id: {course_id}, "
+                         "reason: {reason}".format(user_name=course_enrollment.user.username,
+                                                   course_id=course_enrollment.course_id,
+                                                   reason=e
+                                                   ))
 
 
 def send_re_enroll_email(course_enrollment):
@@ -127,9 +214,11 @@ def send_re_enroll_email(course_enrollment):
             # sending_mail is true means we have to re-enroll the student in the course
             course_re_enroll(course_enrollment)
             course = course_enrollment.course_overview
+            username = course_enrollment.user.first_name or course_enrollment.user.profile.name \
+                       or course_enrollment.user.username
             params = get_email_params(course, True)
             params['finish_days'] = descriptor.course_finish_days
-            print("finish: ", params['finish_days'])
+            params['username'] = username
             params['re_enroll_time'] = descriptor.course_re_enroll_time
             params['time_unit'] = time_unit
             params['email_address'] = course_enrollment.user.email
@@ -181,7 +270,10 @@ def send_reminder_email(course_enrollment):
             days_left = finish_days - delta.days
         time_unit = descriptor.re_enroll_time_unit or 'month'
         course = course_enrollment.course_overview
+        username = course_enrollment.user.first_name or course_enrollment.user.profile.name \
+                   or course_enrollment.user.username
         params = get_email_params(course, True)
+        params['username'] = username
         params['overdue'] = overdue
         params['days_left'] = days_left
         params['finish_days'] = finish_days
