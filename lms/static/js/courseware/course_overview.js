0802db6b22a4e261e5f472862535b3edbe194972
cysean <119115@gmail.com>
2019-10-23 09:39:31 +0800

    Course about/info: optimize code for read more/less (#246)

---
+++ lms/static/js/courseware/course_overview.js
@@ -0,0 +1,20 @@
+
+$(function () {
+    setTimeout(function () {
+      var $courseDescription = $('.course-description');
+      var $description = $('.subtitle-description-wrapper');
+      if ($description.height() >= 172){
+        $courseDescription.addClass('extend-mode');
+      }
+      var $subtitle = $courseDescription.find('.subtitle');
+      if ($subtitle.height()<=16){
+        $courseDescription.addClass('no-title-mode');
+      }
+    }, 2000)
+
+    $('body').delegate('.extend-link', 'click', function (e) {
+      var $src = $(e.currentTarget);
+      var $courseDescription = $src.parent('.course-description');
+      $courseDescription.toggleClass('all-paragraphs');
+    })
+})
