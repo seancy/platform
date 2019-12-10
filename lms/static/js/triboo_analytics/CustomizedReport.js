b6e13f5e312f7b327330fe33eb2a694d9761be26
Sean <se.zhang@learning-tribes.com>
2019-12-10 16:02:07 +0800

    customized report.

--- lms/static/js/triboo_analytics/CustomizedReport.js
+++ lms/static/js/triboo_analytics/CustomizedReport.js
@@ -1,23 +1,70 @@
 /* eslint-disable react/no-danger, import/prefer-default-export */
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'select2'
+import '../../../../node_modules/select2/dist/css/select2.css'
+import {ReactRenderer} from '../../../../common/static/js/src/ReactRenderer'
 
-export class CustomizedReport extends React.Component {
+export class CustomizedReport {
+    constructor(props) {
+        $(() => {
+            new ReactRenderer({
+                component: ReportTypeAndCourseReport,
+                selector: '.report_type_and_course_selected',
+                componentName: 'CustomizedReport',
+                props: props
+            });
+            this.eventInit()
+        })
+    }
+
+    eventInit() {
+        $('input[type=submit]').on('click', (e) => {
+            e.preventDefault();
+            this.synchronizeSelectedCourses();
+            setTimeout(async ()=>{
+                const json = await this.submit()
+                console.log(json)
+                LearningTribes.dialog.show(json.message);
+            },200)
+        })
+    }
+
+    async submit() {
+        let data = {}
+        $('#form-customized-report').serializeArray().forEach(function (x) {
+            data[x.name] = x.value
+        })
+        ///analytics/customized/export/
+        return await $.post({
+            url:'export/',
+            data:data
+        })
+        return await response.json()
+    }
+
+    synchronizeSelectedCourses() {
+        let courseSelectedValueStr = $('#course_selected').select2().val();
+        $('#course_selected_return').val(courseSelectedValueStr)
+    }
+}
+
+class ReportTypeAndCourseReport extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
             hideCourseReportSelect: false
         };
-        $(() => {
-            setTimeout(() => {
-                $(this.refs.course_selected).select2({
-                    multiple: true,
-                    width:'off'
-                });
-            }, 300)
 
-        })
+        $(this.refs.report_type).select2().on('select2:select', this.recreateCourseSelect.bind(this));
+        setTimeout(() => {
+            $(this.refs.course_selected).select2({
+                multiple: true,
+                width: 'off'
+            });
+        }, 300)
+
     }
 
     recreateCourseSelect(e) {
@@ -27,15 +74,15 @@ export class CustomizedReport extends React.Component {
         if (courseReportType == 'multiple' || courseReportType == 'single') {
             isMultiple = courseReportType == 'single' ? false : true;
             $courseSelected.select2({
-                width:'off',
+                width: 'off',
                 multiple: isMultiple
             });
             this.setState({
-                hideCourseReportSelect:false
+                hideCourseReportSelect: false
             })
-        }else{
+        } else {
             this.setState({
-                hideCourseReportSelect:true
+                hideCourseReportSelect: true
             })
         }
     }
@@ -47,13 +94,14 @@ export class CustomizedReport extends React.Component {
             <React.Fragment>
                 <div>
                     <label htmlFor="report_type">{translation.report_type}</label>
-                    <select name="report_type" id="report_type" onChange={this.recreateCourseSelect.bind(this)}>
+                    <select name="report_type" id="report_type" ref="report_type"
+                            onChange={this.recreateCourseSelect.bind(this)}>
                         {report_types.map(({type, title}) => {
                             return <option key={type} value={type}>{title}</option>
                         })}
                     </select>
                 </div>
-                <div className={'course-report ' + (this.state.hideCourseReportSelect?'hide':'')}>
+                <div className={'course-report ' + (this.state.hideCourseReportSelect ? 'hide' : '')}>
                     <label htmlFor="course_selected">{translation.course}</label>
                     <select id="course_selected" ref="course_selected">
                         {courses.map(({cid, course_title}) => {
@@ -66,7 +114,7 @@ export class CustomizedReport extends React.Component {
     }
 }
 
-CustomizedReport.propTypes = {
+CustomizedReport.propTypes = ReportTypeAndCourseReport.propTypes = {
     translation: PropTypes.shape({
         report_type: PropTypes.string,
         course: PropTypes.string
