4a75b1345bf78163e223c30da78ac0fc4955b64a
Sean <se.zhang@learning-tribes.com>
2020-01-22 18:13:44 +0800

    temporary commit.

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -57,6 +57,7 @@ module.exports = Merge.smart({
         StudentAccountDeletionInitializer: ['babel-polyfill','./lms/static/js/student_account/StudentAccountDeletionInitializer.js'],
         CustomizedReport: ['babel-polyfill','./lms/static/js/triboo_analytics/CustomizedReport.js'],
         CourseReport: ['babel-polyfill','./lms/static/js/triboo_analytics/CourseReport.js'],
+        Toolbar: ['babel-polyfill','./lms/static/js/triboo_analytics/Toolbar.js'],
 
         Dialog: ['babel-polyfill','./lms/static/js/dialog.js'],
         Dashboard: ['babel-polyfill','./lms/static/js/dashboard.js'],
