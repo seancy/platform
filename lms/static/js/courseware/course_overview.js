c6ea8083a2afe7a873a4c7d849ae2b1b6770f924
cysean <119115@gmail.com>
2020-04-10 14:54:52 +0800

    course info/about: fix "read more" link (#421)
    Co-authored-by: Sean <se.zhang@learning-tribes.com>

--- lms/static/js/courseware/course_overview.js
+++ lms/static/js/courseware/course_overview.js
@@ -1,24 +1,43 @@
-
 $(function () {
-    setTimeout(function () {
-      var $courseDescription = $('.course-description');
-      var $description = $('.subtitle-description-wrapper');
-      if ($description.height() >= 172){
-        $courseDescription.addClass('extend-mode');
-      }
-      var $subtitle = $courseDescription.find('.subtitle');
-      if ($subtitle.height()<=16){
-        $courseDescription.addClass('no-title-mode');
-      }
-    }, 2000)
 
-    $('body').delegate('.extend-link', 'click', function (e) {
-      var $src = $(e.currentTarget);
-      var $courseDescription = $src.parent('.course-description');
-      $courseDescription.toggleClass('all-paragraphs');
-    })
+    const descriptionInit = function () {
+        let p = new Promise(function (resolve) {
+            setTimeout(function () {
+                var $courseDescription = $('.course-description');
+                var $description = $('.subtitle-description-wrapper');
+                if ($description.height() >= 172) {
+                    $courseDescription.addClass('extend-mode');
+                }
+                var $subtitle = $courseDescription.find('.subtitle');
+                if ($subtitle.height() <= 16) {
+                    $courseDescription.addClass('no-title-mode');
+                }
+                resolve()
+            }, 200)
+        })
+        return p
+    }
 
-    setTimeout(function () {
-        $('.container .intro').removeClass('skeleton')
-    },2500)
+    const skeletonRemove = ()=> {
+        return new Promise(function (resolve) {
+            setTimeout(function () {
+                $('.container .intro').removeClass('skeleton')
+                resolve()
+            }, 100)
+        })
+    }
+
+    const eventInit = () => {
+        $('body').delegate('.extend-link', 'click', function (e) {
+            var $src = $(e.currentTarget);
+            var $courseDescription = $src.parent('.course-description');
+            $courseDescription.toggleClass('all-paragraphs');
+        })
+    }
+
+    // entrance
+    skeletonRemove()
+        .then(descriptionInit)
+        .then(eventInit)
+
 })
