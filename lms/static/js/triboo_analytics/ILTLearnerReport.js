8c1a2d07dad0c854808e7d5867c01125c81d9713
Laetitia Pfaender <l.pfaender@learning-tribes.com>
2020-04-21 21:13:33 +0800

    ilt reports in progress

--- lms/static/js/triboo_analytics/ILTLearnerReport.js
+++ lms/static/js/triboo_analytics/ILTLearnerReport.js
@@ -28,40 +28,40 @@ export default class ILTLearnerReport extends BaseReport {
         return {...{
             fields: [
 
-                {name: 'Geographical area', fieldName: 'Geographical Area'},
-                {name: 'Course country', fieldName: 'Course Country'},
-                {name: 'Zone/Region', fieldName: 'Zone Region'},
-                {name: 'Course tags', fieldName: 'Course Tags'},
+                {name: 'Geographical area', fieldName: 'Geographical area'},
+                {name: 'Course country', fieldName: 'Course country'},
+                {name: 'Zone/Region', fieldName: 'Zone/Region'},
+                {name: 'Course tags', fieldName: 'Course tags'},
 
-                {name: 'Course code', fieldName: 'Course Code'},
-                {name: 'Course name', fieldName: 'Course'},
+                {name: 'Course code', fieldName: 'Course code'},
+                {name: 'Course name', fieldName: 'Course name'},
                 {name: 'Section', fieldName: 'Section'},
                 {name: 'Subsection', fieldName: 'Subsection'},
 
                 {name: 'Session ID', fieldName: 'Session ID'},
-                {name: 'Start date', fieldName: 'Start Date'},
-                {name: 'Start time', fieldName: 'Start Time'},
-                {name: 'End date', fieldName: 'End Date'},
+                {name: 'Start date', fieldName: 'Start date'},
+                {name: 'Start time', fieldName: 'Start time'},
+                {name: 'End date', fieldName: 'End date'},
 
-                {name: 'End time', fieldName: 'EndTime'},
-                {name: 'Duration (in hours)', fieldName: 'Duration'},
+                {name: 'End time', fieldName: 'End time'},
+                {name: 'Duration (in hours)', fieldName: 'Duration (in hours)'},
                 {name: 'Location ID', fieldName: 'Location ID'},
-                {name: 'Location name', fieldName: 'Location Name'},
+                {name: 'Location name', fieldName: 'Location name'},
 
-                {name: 'Location address', fieldName: 'Address'},
-                {name: 'Zip code', fieldName: 'Zip Code'},
+                {name: 'Location address', fieldName: 'Location address'},
+                {name: 'Zip code', fieldName: 'Zip code'},
                 {name: 'City', fieldName: 'City'},
                 {name: 'Name', fieldName: 'Name'},
 
                 ...propertiesFields,
 
-                {name: 'Enrollment status', fieldName: 'Enrollment Status'},
+                {name: 'Enrollment status', fieldName: 'Enrollment status'},
                 {name: 'Attendee', fieldName: 'Attendee'},
-                {name: 'Outward trips', fieldName: 'Outward Trips'},
-                {name: 'Return trips', fieldName: 'Return Trips'},
+                {name: 'Outward trips', fieldName: 'Outward trips'},
+                {name: 'Return trips', fieldName: 'Return trips'},
 
-                {name: 'Overnight stay', fieldName: 'Overnight Stay'},
-                {name: 'Overnight stay address', fieldName: 'Overnight Stay Address'},
+                {name: 'Overnight stay', fieldName: 'Overnight stay'},
+                {name: 'Overnight stay address', fieldName: 'Overnight stay address'},
                 {name: 'Comment', fieldName: 'Comment'}
             ],
         }, ...this.getBaseConfig()}
