18cf834d7006da8830813e85e13810b8a8cf74ae
rythmE <poiuwertion@gmail.com>
2020-05-28 15:52:19 +0800

    Course duration on catalogue course cards (#456)

--- cms/djangoapps/contentstore/courseware_index.py
+++ cms/djangoapps/contentstore/courseware_index.py
@@ -575,6 +575,7 @@ class CourseAboutSearchIndexer(object):
         AboutInfo("title", AboutInfo.ANALYSE | AboutInfo.PROPERTY, AboutInfo.FROM_ABOUT_INFO),
         AboutInfo("university", AboutInfo.ANALYSE | AboutInfo.PROPERTY, AboutInfo.FROM_ABOUT_INFO),
         AboutInfo("number", AboutInfo.ANALYSE | AboutInfo.PROPERTY, AboutInfo.FROM_COURSE_PROPERTY),
+        AboutInfo("duration", AboutInfo.ANALYSE, AboutInfo.FROM_ABOUT_INFO),
         AboutInfo("short_description", AboutInfo.ANALYSE, AboutInfo.FROM_ABOUT_INFO),
         AboutInfo("description", AboutInfo.ANALYSE, AboutInfo.FROM_ABOUT_INFO),
         AboutInfo("key_dates", AboutInfo.ANALYSE, AboutInfo.FROM_ABOUT_INFO),
