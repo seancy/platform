6f1ef633ffe5622f423f55ab3197e01125c6cd0a
Aaron Zhao <rudy710@qq.com>
2020-07-27 15:37:16 +0800

    Leaderboard: ES, PT translations (#514)

--- scripts/leaderboard_sql/leaderboard_view.sql
+++ scripts/leaderboard_sql/leaderboard_view.sql
@@ -1,10 +1,16 @@
-CREATE VIEW `triboo_analytics_leaderboardview` AS
+CREATE OR REPLACE VIEW `triboo_analytics_leaderboardview` AS
   SELECT
     `triboo_analytics_leaderboard`.`id` AS `id`,
     `triboo_analytics_leaderboard`.`user_id` AS `user_id`,
     `triboo_analytics_leaderboard_total`.`total_score` AS `total_score`,
-    (`triboo_analytics_leaderboard_total`.`total_score` - `triboo_analytics_leaderboard`.`last_week_score`) AS `current_week_score`,
-    (`triboo_analytics_leaderboard_total`.`total_score` - `triboo_analytics_leaderboard`.`last_month_score`) AS `current_month_score`,
+    CASE
+      WHEN `triboo_analytics_leaderboard_total`.`total_score` < `triboo_analytics_leaderboard`.`last_week_score` THEN 0
+      ELSE `triboo_analytics_leaderboard_total`.`total_score` - `triboo_analytics_leaderboard`.`last_week_score`
+    END AS `current_week_score`,
+    CASE
+      WHEN `triboo_analytics_leaderboard_total`.`total_score` < `triboo_analytics_leaderboard`.`last_month_score` THEN 0
+      ELSE `triboo_analytics_leaderboard_total`.`total_score` - `triboo_analytics_leaderboard`.`last_month_score`
+    END AS `current_month_score`,
     `triboo_analytics_leaderboard`.`last_week_rank` AS `last_week_rank`,
     `triboo_analytics_leaderboard`.`last_month_rank` AS `last_month_rank`,
     `triboo_analytics_leaderboard`.`modified` AS `last_updated`
