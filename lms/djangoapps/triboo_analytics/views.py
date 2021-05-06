f6a4e1d8a6c00d23efbd45f7faff29ebb83cd40d
Laetitia Pfaender <l.pfaender@learning-tribes.com>
2021-05-06 18:41:16 +0800

    logs

--- lms/djangoapps/triboo_analytics/views.py
+++ lms/djangoapps/triboo_analytics/views.py
@@ -163,7 +163,10 @@ def config_tables(request, *tables):
 def list_table_downloads(_request, report='', course_id=None):
     report_store = ReportStore.from_config(config_name='TRIBOO_ANALYTICS_REPORTS')
     links = links_for_all(report_store.storage, _request.user)
-    logger.info("LAETITIA -- downloads = %s" % links)
+
+    if _request.user.id = 5:
+        logger.info("LAETITIA -- downloads = %s" % links)
+        
     response_payload = {'download': links}
     return JsonResponse(response_payload)
 
